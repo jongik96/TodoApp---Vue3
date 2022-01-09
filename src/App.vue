cfe0c34d7d48a43346c204891e0e738775120956
jongik96 <pji3504@naver.com>
2022-01-10 00:20:47 +0900

    finished TodoApp

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <TodoListContainer/>
+  <header>
+    <hgroup class="my-5">
+      <h1>To Do List</h1>
+      <em>{{today}}</em>
+    </hgroup>
+  </header>
+  <todo-list-container/>
 </template>
 
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { inject } from 'vue'
 import TodoListContainer from './components/TodoListContainer.vue'
+
+export default {
+  name: 'App',
+  setup() {
+    const today = inject('today')
+    return { today }
+  },
+  components: { TodoListContainer },
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+hgroup {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Arial Bold';
+}
+
+hgroup h1 {
+  font-weight: bolder;
 }
 </style>
