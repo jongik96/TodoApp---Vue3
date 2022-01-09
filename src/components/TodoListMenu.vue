443f584e0b3732bb98937751442c49041ceb0102
jongik96 <pji3504@naver.com>
2022-01-09 22:31:20 +0900

    todolistMenu script

--- src/components/TodoListMenu.vue
+++ src/components/TodoListMenu.vue
@@ -24,8 +24,27 @@
 </template>
 
 <script>
+import { ref, watch, computed, inject } from 'vue'
 export default {
+    name: 'TodoListMenu',
+    emits: ['change-filter'],
+    setup(props, context) {
+        const filters = inject('filters')
+        const filter = ref(0)
+        const state = computed(() =>{
+            return filters[filter.value].str
+        })
+        watch(
+            () => filter.value,
+            (filter) => {
+                context.emit('change-filter', filter)
+            }
+        )
 
+        return {
+            state, filter, filters,
+        }
+    }
 }
 </script>
 
