968fa5d665f2fdea6104cd06c3c0710b1823d22f
jongik96 <pji3504@naver.com>
2022-01-09 18:57:25 +0900

    todoList markup

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,45 @@
 <template>
-  
+  <main>
+      <div v-for="(todo, idx) in data" :key="todo.id">
+          <div class="input-group my-2 input-group-sm">
+              <div class="input-group-text">
+                  <input
+                   type="checkbox"
+                   class="form-check-input mt-0" 
+                   :checked="todo.completed" 
+                   :disabled="todo.completed" 
+                   @click="completeTodo(todo.id)"
+                    >
+              </div>
+              <div class="input-group-text">
+                  <input
+                    class="form-input mt-0"
+                    type="date"
+                    :min="today"
+                    disabled
+                    :value="todo.date"
+                  >
+              </div>
+              <input
+               type="text"
+               class="form-control"
+               :value="todo.job"
+               >
+              <button class="btn btn-outline-primary dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+              >
+              할 일 관리
+              </button>
+              <ul class="dropdown-menu dropdown-menu-end">
+                  <li v-for="item in menu" :key="item.str">
+                      {{item.str}}
+                  </li>
+              </ul>
+          </div>
+          <div v-show="idx + 1 < data.length" class="col border border-second"/>
+      </div>
+  </main>
 </template>
 
 <script>
